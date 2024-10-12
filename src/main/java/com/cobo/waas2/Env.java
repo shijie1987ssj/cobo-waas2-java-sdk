31a22b2fc0a29e7017cd97bf29eae81e61d621d4
shijie.sun <shijie.sun@antalpha.com>
2024-10-12 19:09:18 +0800

    cobo 公钥

--- src/main/java/com/cobo/waas2/Env.java
+++ src/main/java/com/cobo/waas2/Env.java
@@ -4,4 +4,6 @@ public class Env {
     public static Integer DEV = 0;
     public static Integer PROD = 1;
 
+    public static String DevPubKey = "03596da539963fb1dd29d5859e25903eb76b9f7ed2d58516e29c9f80c201ff2c1b";
+    public static String ProdPubKey = "02c3e5bacf436fbf4da78597e791579f022a2e85073ae36c54a361ff97f2811376";
 }
