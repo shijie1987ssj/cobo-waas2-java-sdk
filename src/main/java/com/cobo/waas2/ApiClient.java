f3cf8ebacb3d7c4fbf707e928778dc9967788bbe
Shanshan Li <shanshan.li@cobo.com>
2024-12-16 15:18:32 +0800

    release java v1.6.2

--- src/main/java/com/cobo/waas2/ApiClient.java
+++ src/main/java/com/cobo/waas2/ApiClient.java
@@ -195,7 +195,7 @@ public class ApiClient {
         json = new JSON();
 
         // Set default User-Agent.
-        setUserAgent("cobo-waas2-java-sdk/1.6.1");
+        setUserAgent("cobo-waas2-java-sdk/1.6.2");
 
         authentications = new HashMap<String, Authentication>();
     }
